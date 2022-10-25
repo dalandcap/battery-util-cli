e0334e0108254aedd50afa110aa40e16a6300c1d
Daniel Filat <filatdaniel98@gmail.com>
2022-10-25 17:39:20 +0300

    stores battery logs in a state with battery level and time

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 const batteryLevel = require('battery-level');
 
-const FIVE_MINUTES_IN_MILISECONDS = 1000 * 60 * 5;
+interface BatteryLog {
+  time: unknown;
+  level: number;
+}
+
+const FIVE_MINUTES_IN_MILLISECONDS = 1000 * 60;
+
+let batteryLogs: BatteryLog[] = [];
 
 const logBattery = () =>
   console.log(
     batteryLevel().then((level: number) => {
       console.log(level);
+      batteryLogs = [...batteryLogs, { time: Date.now(), level }];
     })
   );
 
-setInterval(logBattery, FIVE_MINUTES_IN_MILISECONDS);
+setInterval(logBattery, FIVE_MINUTES_IN_MILLISECONDS);
